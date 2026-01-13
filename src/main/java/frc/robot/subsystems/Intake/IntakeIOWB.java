c1df80e3207e92d2d674fe0db9f10e78a7483f2b
Kaleb-Robotics <26BallmannKaleb@bprep.org>
2026-01-12 19:07:53 -0800

    initial commit

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOWB.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Intake;
+
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeIOWB extends SubsystemBase {
+  private final TalonFX intakeMotor = new TalonFX(0, "default name");
+  private final TalonFXConfiguration talonFXconfigs = new TalonFXConfiguration();
+  private final MotorOutputConfigs motorOutputConfigs = new MotorOutputConfigs();
+  /** Creates a new IntakeIOWB. */
+  public IntakeIOWB() {
+
+  }
+
+  public void setDutyCycle(double duty) {
+    intakeMotor.set(duty);
+  }
+
+  public void stop() {
+    intakeMotor.stopMotor();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
