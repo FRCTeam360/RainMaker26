c47cc118d7b395827062cb77d7659d263ad7e4eb
RaineyRubin <145077026+RaineyRubin@users.noreply.github.com>
2026-01-14 20:49:56 -0800

    Adding in more of the flywheel and advantage kit

--- src/main/java/frc/robot/subsystems/Flywheel/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel/Flywheel.java
@@ -4,19 +4,32 @@
 
 package frc.robot.subsystems.Flywheel;
 
+import com.revrobotics.spark.SparkBase.ControlType;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Flywheel extends SubsystemBase {
   private final FlywheelIO io;
+  private double rpmSetpoint = 0.0;
+  private final FlywheelIOInputsAutoLogged inputs = new FlywheelIOInputsAutoLogged();
 
   /** Creates a new Flywheel. */
   public Flywheel(FlywheelIO io) {
     this.io = io;
   }
+  public void run(double speed){
+    io.setSpeed(speed);
+  }
+  public void setRPM(double rpm) {
+    rpmSetpoint = rpm;
+    io.setRPM(rpm, ControlType.kVelocity);
+  }
+
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    io.updateInputs(inputs);
   }
 
   public void setDutyCycle(double duty) {
@@ -26,4 +39,25 @@ public class Flywheel extends SubsystemBase {
   public void stop() {
     io.setDutyCycle(0.0);
   }
+  public double getPower(){
+    return io.getPower();
+  }
+  public double getVelocity(){
+    return io.getVelocity();
+  }
+   public boolean isAtSetpoint() {
+    return Math.abs(this.getVelocity() - rpmSetpoint) < 215.0;
+  }
+   public boolean isAboveSetpoint() {
+    return this.getVelocity() >= rpmSetpoint-100.0;
+  }
+
+  public boolean isBelowSetpoint() {
+    return this.getVelocity() <= rpmSetpoint - 30.0;
+  }
+
+    public Command setDutyCycleCmd(double duty) {
+    return this.runEnd(() -> io.setDutyCycle(duty), () -> io.setDutyCycle(0.0));
+  }
+  
 }
