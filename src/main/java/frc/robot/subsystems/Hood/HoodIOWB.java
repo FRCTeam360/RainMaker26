ea2dfc36794806b61123b19d940cc5efc4f1ea8a
27MahoneyKeirnan <142344414+27MahoneyKeirnan@users.noreply.github.com>
2026-01-16 18:48:20 -0800

    Hood subsystem(hood motor unknown by CAD)

--- src/main/java/frc/robot/subsystems/Hood/HoodIOWB.java
+++ src/main/java/frc/robot/subsystems/Hood/HoodIOWB.java
@@ -4,14 +4,42 @@
 
 package frc.robot.subsystems.Hood;
 
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
-public class HoodIOWB {
+public class HoodIOWB implements HoodIO {
   /** Creates a new HoodIOWB. */
-  public HoodIOWB() {}
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  private final SparkMax hoodMotor = new SparkMax(Constants.WoodBotConstants.HOOD_ID, MotorType.kBrushless);
+  private final RelativeEncoder encoder = hoodMotor.getEncoder();
+  private final SparkMaxConfig sparkMaxConfig = new SparkMaxConfig();
+
+  public HoodIOWB() {
+    sparkMaxConfig.idleMode(IdleMode.kBrake);
+    sparkMaxConfig.inverted(false);
+    // CAD doesn't know what motor type it is, we set to assume sparkmax.
+
+    hoodMotor.configure(sparkMaxConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public void updateInputs(HoodIOInputs inputs) {
+    inputs.hoodPosition = encoder.getPosition();
+    inputs.hoodStatorCurrent = hoodMotor.getOutputCurrent();
+    inputs.hoodSupplyCurrent = hoodMotor.getOutputCurrent() * hoodMotor.getAppliedOutput(); // TODO: check if this is
+                                                                                            // right
+    inputs.hoodVelocity = encoder.getVelocity();
+    inputs.hoodVoltage = hoodMotor.getBusVoltage() * hoodMotor.getAppliedOutput();
+  }
+
+  public void setDutyCycle(double dutyCycle) {
+    hoodMotor.set(dutyCycle);
   }
 }
