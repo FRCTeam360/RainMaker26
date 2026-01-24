00ab770900682d0e27575e54dae9c315acc4cc55
James Taylor <james.taylor9109@protonmail.com>
2026-01-23 23:58:53 -0800

    Publishing tests of alliance data retrieval

--- src/main/java/frc/robot/utils/RobotUtils.java
+++ src/main/java/frc/robot/utils/RobotUtils.java
@@ -1,5 +1,7 @@
 package frc.robot.utils;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import java.io.File;
 
 public class RobotUtils {
@@ -15,6 +17,28 @@ public class RobotUtils {
         System.out.println("usb not found");
       }
     }
+    return false;
+  }
+
+  public static Alliance getAutoWinner() {
+    String gameData = DriverStation.getGameSpecificMessage();
+
+    if (gameData.length() > 0) {
+      switch (gameData.charAt(0)) {
+        case 'B':
+          // Blue won AUTO - Blue hub inactive in Shift 1 & 3
+          return Alliance.Blue;
+        case 'R':
+          // Red won AUTO - Red hub inactive in Shift 1 & 3
+          return Alliance.Red;
+        default:
+          // Corrupt data
+      }
+    }
+    return null;
+  }
+
+  public static boolean canShoot(double estTOF) {
     return false;
   }
 }
