5174219d326f6708f8b29e16eb052044545e897d
JakeParker <ilikebuffalowingsalot@gmail.com>
2026-01-24 16:27:50 -0800

    stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,8 +17,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.BasicIntakeCommand;
-import frc.robot.commands.BasicShootCommand;
 import frc.robot.generated.WoodBotDrivetrain;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Flywheel.Flywheel;
@@ -53,7 +51,6 @@ public class RobotContainer {
   private Intake intake;
   private IntakePivot intakePivot;
   private FlywheelKicker flywheelKicker;
-  private CommandFactory commandFactory;
 
 
 
@@ -142,11 +139,14 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
-    driverCont.leftBumper().whileTrue(commandFactory.basicIntakeCmd());
-    driverCont.rightBumper().whileTrue(commandFactory.basicShootCmd());
-    driverCont.a().whileTrue(intake.setDutyCycleCommand(1.0));
-    
+    basicIntakeCommand = new BasicIntakeCommand(intake, indexer, flywheelKicker);
+    basicShootCommand = new BasicShootCommand(flywheel);
+    driverCont.leftBumper().whileTrue(basicIntakeCommand);
+    driverCont.rightBumper().whileTrue(basicShootCommand);
+    driverCont.a().whileTrue(intake.setDutyCycleCommand(()-> 1.0));
+
     drivetrain.setDefaultCommand(drivetrain.fieldOrientedDrive(driverCont));
+    
     drivetrain.registerTelemetry(logger::telemeterize);
 
   }
