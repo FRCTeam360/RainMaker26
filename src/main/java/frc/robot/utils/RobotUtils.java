7778b82fda3d7b614a12793644c6964724562e0b
Okim29 <29kimowen@bprep.org>
2026-01-28 18:28:03 -0800

    Added function to check which hub is open

--- src/main/java/frc/robot/utils/RobotUtils.java
+++ src/main/java/frc/robot/utils/RobotUtils.java
@@ -2,6 +2,9 @@ package frc.robot.utils;
 
 import java.io.File;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 public class RobotUtils {
   public static boolean isUsbWriteable() {
     File usb = new File("/U");
@@ -16,5 +19,23 @@ public class RobotUtils {
       }
     }
     return false;
+  }
+  public static Alliance hubData() {
+    String hubData = DriverStation.getGameSpecificMessage();
+    if(hubData.length() > 0){
+      char allianceChar = hubData.charAt(0);
+      //checks which hub is open
+      switch(hubData.charAt(0)){
+        case 'B':
+        return Alliance.Blue;
+        case 'R':
+        return Alliance.Red;
+        default:
+        //only called when there's an invalid character for the game specific message
+        throw new IllegalStateException("Invalid character for alliance configuration from game specific message: " + allianceChar);
+      }
+    } 
+    //called when no data was received from driver station
+    throw new IllegalStateException("Driver station could not receive game specific message");
   }
 }
