b9c8eeb355453b1519eb8547b75e5fbfc9b5003b
JakeParker <ilikebuffalowingsalot@gmail.com>
2026-01-28 18:09:41 -0800

    Changed getState() to getStateCopy()

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -223,14 +223,13 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   }
 
   private void configureAutoBuilder() {
-
     try {
       var config = RobotConfig.fromGUISettings();
 
       AutoBuilder.configure(
-          () -> getState().Pose, // Supplier of current robot pose
+          () -> getStateCopy().Pose, // Supplier of current robot pose
           this::resetPose, // Consumer for seeding pose against auto
-          () -> getState().Speeds, // Supplier of current robot speeds
+          () -> getStateCopy().Speeds, // Supplier of current robot speeds
           // Consumer of ChassisSpeeds and feedforwards to drive the robot
           (speeds, feedforwards) ->
               setControl(
