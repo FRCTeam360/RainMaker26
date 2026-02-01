959bb4bc97aa158fa1abf72e2cba40ed2e9d34d5
James Taylor <james.taylor9109@protonmail.com>
2026-01-31 23:37:11 -0800

    Reformatted files

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RainMaker26";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 291;
-  public static final String GIT_SHA = "2e10a014c4cd9c0dbc363db7d77b255c13a17a68";
-  public static final String GIT_DATE = "2026-01-31 22:45:00 EST";
-  public static final String GIT_BRANCH = "Rename-Indexer-to-Hopper";
-  public static final String BUILD_DATE = "2026-01-31 22:51:23 EST";
-  public static final long BUILD_UNIX_TIME = 1769917883546L;
+  public static final int GIT_REVISION = 309;
+  public static final String GIT_SHA = "e52023ed1e17b5590035cdcfb0255cb2fc7b7748";
+  public static final String GIT_DATE = "2026-02-01 02:36:17 EST";
+  public static final String GIT_BRANCH = "photon_swerve_pose_testing";
+  public static final String BUILD_DATE = "2026-02-01 02:36:45 EST";
+  public static final long BUILD_UNIX_TIME = 1769931405458L;
   public static final int DIRTY = 0;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
