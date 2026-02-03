cbd7f3a9875ae16d06cdb9f57a32e707490d0dbc
RaineyRubin <145077026+RaineyRubin@users.noreply.github.com>
2026-02-02 18:59:57 -0800

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "RainMaker26";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 310;
+  public static final String GIT_SHA = "9c450f5ebeedf5ec780ad147c92dc72689009c76";
+  public static final String GIT_DATE = "2026-02-02 20:06:28 EST";
+  public static final String GIT_BRANCH = "Autos";
+  public static final String BUILD_DATE = "2026-02-02 21:55:35 EST";
+  public static final long BUILD_UNIX_TIME = 1770087335584L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
