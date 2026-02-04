74217d06505b1eaf4e3f46fca662c7156d3a0d78
James Taylor <james.taylor9109@protonmail.com>
2026-02-03 20:17:00 -0800

    Partial revert to changes in hood

--- src/main/java/frc/robot/subsystems/Hood/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood/Hood.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems.Hood;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
@@ -12,6 +13,7 @@ import org.littletonrobotics.junction.Logger;
 public class Hood extends SubsystemBase {
   private final HoodIO io;
   private final HoodIOInputsAutoLogged inputs = new HoodIOInputsAutoLogged();
+  private final double TOLERANCE = 0.5;
 
   public enum HoodStates {
     OFF,
@@ -40,10 +42,6 @@ public class Hood extends SubsystemBase {
     }
   }
 
-  public double getPosition() {
-    return inputs.position;
-  }
-
   private void updateState() {
     previousState = currentState;
 
@@ -63,10 +61,6 @@ public class Hood extends SubsystemBase {
     this.io = io;
   }
 
-  public boolean atSetpoint(double setpoint) {
-    return Math.abs(getPosition() - setpoint) < 0.5;
-  }
-
   public void setDutyCycle(double dutyCycle) {
     io.setDutyCycle(dutyCycle);
   }
@@ -75,9 +69,13 @@ public class Hood extends SubsystemBase {
     io.setPosition(position);
   }
 
-  // public Command setPositionCmd(double position) {
-  //   return this.setPosition(() -> );
-  // }
+  public double getPosition() {
+    return inputs.position;
+  }
+
+  public Command setPositionCmd(double position) {
+    return this.runOnce(() -> io.setPosition(position));
+  }
 
   public void setEncoder(double position) {
     io.setEncoder(position);
@@ -85,6 +83,18 @@ public class Hood extends SubsystemBase {
 
   public void stop() {
     io.setDutyCycle(0);
+  }
+
+  public boolean atSetpoint(double setpoint) {
+    return Math.abs(getPosition() - setpoint) < TOLERANCE;
+  }
+
+  public Command moveToZeroAndZero() {
+    return Commands.waitUntil(
+            () -> Math.abs(inputs.supplyCurrent) >= 30.0 && Math.abs(inputs.velocity) == 0.0)
+        .deadlineFor(this.runEnd(() -> io.setDutyCycle(0.1), () -> io.setDutyCycle(0.0)))
+        // TODO make this call this.zero()
+        .andThen(runOnce(() -> inputs.position = 0.0));
   }
 
   @Override
@@ -103,5 +113,9 @@ public class Hood extends SubsystemBase {
 
   public Command setDutyCycleCommand(DoubleSupplier valueSup) {
     return this.runEnd(() -> io.setDutyCycle(valueSup.getAsDouble()), () -> io.setDutyCycle(0.0));
+  }
+
+  public Command zero() {
+    return this.runOnce(() -> setEncoder(0.0));
   }
 }
