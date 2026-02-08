5ab16c3a5889254b62e70ded7be67cb05b8f0524
James Taylor <james.taylor9109@protonmail.com>
2026-02-08 13:46:50 -0800

    Replaced AllianceFlipUtil to make hub distance tracking work accurately

--- src/main/java/frc/robot/subsystems/Shooter/AllianceFlipUtil.java
+++ src/main/java/frc/robot/subsystems/Shooter/AllianceFlipUtil.java
@@ -1,102 +1,47 @@
 package frc.robot.subsystems.Shooter;
 
-// Copyright (c) 2023 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
-// package org.littletonrobotics.frc2023.util;
-
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-// import org.littletonrobotics.frc2023.FieldConstants;
-// import org.littletonrobotics.frc2023.util.trajectory.RotationSequence;
 import frc.robot.utils.FieldConstants;
 
-/**
- * Utility functions for flipping from the blue to red alliance. By default, all translations and
- * poses in {@link FieldConstants} are stored with the origin at the rightmost point on the blue
- * alliance wall.
- */
 public class AllianceFlipUtil {
-  /** Flips a translation to the correct side of the field based on the current alliance color. */
+  public static double applyX(double x) {
+    return shouldFlip() ? FieldConstants.fieldLength - x : x;
+  }
+
+  public static double applyY(double y) {
+    return shouldFlip() ? FieldConstants.fieldWidth - y : y;
+  }
+
   public static Translation2d apply(Translation2d translation) {
-    if (shouldFlip()) {
-      return new Translation2d(FieldConstants.fieldLength - translation.getX(), translation.getY());
-    } else {
-      return translation;
-    }
+    return new Translation2d(applyX(translation.getX()), applyY(translation.getY()));
   }
 
-  /** Flips an x coordinate to the correct side of the field based on the current alliance color. */
-  public static double apply(double xCoordinate) {
-    if (shouldFlip()) {
-      return FieldConstants.fieldLength - xCoordinate;
-    } else {
-      return xCoordinate;
-    }
-  }
-
-  /** Flips a rotation based on the current alliance color. */
   public static Rotation2d apply(Rotation2d rotation) {
-    if (shouldFlip()) {
-      return new Rotation2d(-rotation.getCos(), rotation.getSin());
-    } else {
-      return rotation;
-    }
+    return shouldFlip() ? rotation.rotateBy(Rotation2d.kPi) : rotation;
   }
 
-  /** Flips a pose to the correct side of the field based on the current alliance color. */
   public static Pose2d apply(Pose2d pose) {
-    if (shouldFlip()) {
-      return new Pose2d(
-          FieldConstants.fieldLength - pose.getX(),
-          pose.getY(),
-          new Rotation2d(-pose.getRotation().getCos(), pose.getRotation().getSin()));
-    } else {
-      return pose;
-    }
+    return shouldFlip()
+        ? new Pose2d(apply(pose.getTranslation()), apply(pose.getRotation()))
+        : pose;
   }
 
-  /**
-   * Flips a trajectory state to the correct side of the field based on the current alliance color.
-   */
-  public static Trajectory.State apply(Trajectory.State state) {
-    if (shouldFlip()) {
-      return new Trajectory.State(
-          state.timeSeconds,
-          state.velocityMetersPerSecond,
-          state.accelerationMetersPerSecondSq,
-          new Pose2d(
-              FieldConstants.fieldLength - state.poseMeters.getX(),
-              state.poseMeters.getY(),
-              new Rotation2d(
-                  -state.poseMeters.getRotation().getCos(),
-                  state.poseMeters.getRotation().getSin())),
-          -state.curvatureRadPerMeter);
-    } else {
-      return state;
-    }
+  public static Translation3d apply(Translation3d translation) {
+    return new Translation3d(
+        applyX(translation.getX()), applyY(translation.getY()), translation.getZ());
   }
 
-  /** Flips a rotation sequence state based on the current alliance color. */
-  public static RotationSequence.State apply(RotationSequence.State state) {
-    if (shouldFlip()) {
-      return new RotationSequence.State(
-          new Rotation2d(-state.position.getCos(), state.position.getSin()),
-          -state.velocityRadiansPerSec);
-    } else {
-      return state;
-    }
+  public static Rotation3d apply(Rotation3d rotation) {
+    return shouldFlip() ? rotation.rotateBy(new Rotation3d(0.0, 0.0, Math.PI)) : rotation;
   }
 
-  private static boolean shouldFlip() {
-    return DriverStation.getAlliance().equals(Alliance.Red);
+  public static Pose3d apply(Pose3d pose) {
+    return new Pose3d(apply(pose.getTranslation()), apply(pose.getRotation()));
+  }
+
+  public static boolean shouldFlip() {
+    return DriverStation.getAlliance().isPresent()
+        && DriverStation.getAlliance().get() == DriverStation.Alliance.Red;
   }
 }
