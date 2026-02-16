ba693dbc3e57e4b575ede008bf0d66d9e06408a1
Kaleb-Robotics <26BallmannKaleb@bprep.org>
2026-02-15 16:03:49 -0800

    undid auto changes and excess, unused code

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -144,7 +144,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     // Configure the heading controller with PID values
 
-    headingSupplier.get();
     m_faceHubRequest.HeadingController.setPID(HEADING_KP, HEADING_KI, HEADING_KD);
     m_faceHubRequest.HeadingController.enableContinuousInput(-Math.PI, Math.PI);
     m_faceHubRequest.HeadingController.setIZone(HEADING_I_ZONE);
