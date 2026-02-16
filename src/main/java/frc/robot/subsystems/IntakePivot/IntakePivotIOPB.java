795d7451fba617bda603248ee08831b5527afc35
Anna Shim <122415457+annashimshock@users.noreply.github.com>
2026-02-16 14:28:47 -0800

    used 2024 code to make intakepivot

--- src/main/java/frc/robot/subsystems/IntakePivot/IntakePivotIOPB.java
+++ src/main/java/frc/robot/subsystems/IntakePivot/IntakePivotIOPB.java
@@ -6,25 +6,51 @@ package frc.robot.subsystems.IntakePivot;
 
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.spark.config.SoftLimitConfig;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.Constants;
-import frc.robot.subsystems.IntakePivot.IntakePivotIO.IntakePivotIOInputs;
 
 public class IntakePivotIOPB implements IntakePivotIO {
   private final TalonFX intakePivot =
       new TalonFX(Constants.WoodBotConstants.INTAKE_PIVOT_ID, Constants.WoodBotConstants.CANBUS);
+
+  private DutyCycleOut dutyCycleOut = new DutyCycleOut(0);
+
   private final TalonFXConfiguration config = new TalonFXConfiguration();
   private final CurrentLimitsConfigs currentLimitConfig = new CurrentLimitsConfigs();
   private final SoftLimitConfig softLimitConfig = new SoftLimitConfig();
+  private NeutralModeValue neutralMode = NeutralModeValue.Brake;
+
+  private DigitalInput zeroButton;
+  private DigitalInput brakeButton;
+  MotionMagicConfigs motionMagic ;
+
+  private boolean zeroPrev = false;
+  private boolean brakePrev = false;
+
+// TODO: UPDATE GEAR RATIO
+  private final double GEAR_RATIO = 360.0 / 60.0;
+//TODO: ADD CONVERSION FACTOR TO GET MECHANISM INTO DEGREES
+
 
   /** Creates a new IntakePivotIOWB. */
   public IntakePivotIOPB() {
-    MotionMagicConfigs motionMagic = new MotionMagicConfigs();
     intakePivot.getConfigurator().apply(config);
     intakePivot.setNeutralMode(NeutralModeValue.Brake);
+
+    final double motionMagicAcceleration = 400.0;
+    final double motionMagicCruiseVelocity = 85.0;
+    final double motionMagicCruiseJerk = 1750.0;
+
+    final double forwardLimit = 178.0; // TODO: make sure these are correct for prac bot
+    final double reverseLimit = 0.0; // 29.5
 
     currentLimitConfig.StatorCurrentLimit = 120.0;
     currentLimitConfig.SupplyCurrentLimit = 60.0;
@@ -39,18 +65,84 @@ public class IntakePivotIOPB implements IntakePivotIO {
     final double kD = 0.0;
     final double kA = 0.0;
     final double kG = 0.0;
-    final double kFF = 0.0;
+    final double kS = 0.0;
+    final double kV = 0.0;
 
-    intakePivot.getConfigurator().apply(currentLimitConfig);
+    Slot0Configs slot0Configs = config.Slot0;
+    slot0Configs.kA = kA;
+    slot0Configs.kD = kD;
+    slot0Configs.kG = kG;
+    slot0Configs.kI = kI;
+    slot0Configs.kP = kP;
+    slot0Configs.kS = kS;
+    slot0Configs.kV = kV;
+
+    MotionMagicConfigs motionMagicConfigs = config.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity = motionMagicCruiseVelocity;
+    motionMagicConfigs.MotionMagicAcceleration = motionMagicAcceleration;
+    motionMagicConfigs.MotionMagicJerk = motionMagicCruiseJerk;
+
+    config.Voltage.PeakForwardVoltage = 12.0;
+    config.Voltage.PeakReverseVoltage = 12.0;
+
+    config.SoftwareLimitSwitch.withForwardSoftLimitThreshold(forwardLimit)
+        .withReverseSoftLimitThreshold(reverseLimit)
+        .withForwardSoftLimitEnable(true)
+        .withReverseSoftLimitEnable(true);
+
+    config.MotionMagic.withMotionMagicAcceleration(motionMagicAcceleration)
+        .withMotionMagicCruiseVelocity(motionMagicCruiseVelocity)
+        .withMotionMagicJerk(motionMagicCruiseJerk);
+
+    intakePivot.getConfigurator().apply(config, 0.050);
   }
 
-  public void setPosition(double value) {
-    intakePivot.setPosition(value);
+
+  public void setPosition(double position) {
+    MotionMagicVoltage motionMagicVoltage = new MotionMagicVoltage(position);
+    intakePivot.setControl(motionMagicVoltage);
   }
 
   public void setDutyCycle(double value) {
-    intakePivot.set(value);
+    intakePivot.setControl(dutyCycleOut.withOutput(value));
   }
+
+  public void enableBrakeMode(){
+    neutralMode = NeutralModeValue.Brake;
+    intakePivot.setNeutralMode(NeutralModeValue.Brake);
+  }
+  public void disableBrakeMode(){
+    neutralMode = NeutralModeValue.Coast;
+    intakePivot.setNeutralMode(NeutralModeValue.Coast);
+  }
+
+  public boolean isBrakeMode(){
+    return neutralMode == NeutralModeValue.Brake;
+  }
+
+//TODO: ASK ELECTRICAL FOR A ZEROING BUTTON OR AN ABSOLUTE ENCODER
+  private boolean getRawZeroButton(){
+    return !this.zeroButton.get();
+  }
+
+  public boolean getZeroButton(){
+    boolean zeroCurr = getRawZeroButton();
+    boolean risingEdge = zeroCurr && !zeroPrev;
+    zeroPrev = zeroCurr;
+    return risingEdge;
+  }
+
+  private boolean getRawBrakeButton(){
+    return !this.brakeButton.get();
+  }
+
+  public boolean getBrakeButton(){
+    boolean brakeCurr = getRawBrakeButton();
+    boolean risingEdge = brakeCurr && !brakePrev;
+    brakePrev = brakeCurr;
+    return risingEdge;
+  }
+
 
   public void updateInputs(IntakePivotIOInputs inputs) {
     inputs.position = intakePivot.getPosition().getValueAsDouble();
