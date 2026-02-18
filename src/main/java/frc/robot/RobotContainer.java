62298ce28ebb2d925a614a1e3ac6346ef573934c
James Taylor <james.taylor9109@protonmail.com>
2026-02-17 18:06:43 -0800

    Added time based upper limit for auto shoot command execution

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -145,11 +146,15 @@ public class RobotContainer {
       // inputs)
       registerPathplannerCommand(
           "shoot at hub",
-          superStructure
-              .setStateCommand(SuperStates.SHOOTING)
-              .alongWith(
-                  drivetrain.faceAngleWhileDrivingCommand(
-                      () -> 0, () -> 0, () -> shotCalculator.calculateShot().targetHeading())));
+          Commands.waitSeconds(10)
+              .deadlineFor(
+                  superStructure
+                      .setStateCommand(SuperStates.SHOOTING)
+                      .alongWith(
+                          drivetrain.faceAngleWhileDrivingCommand(
+                              () -> 0,
+                              () -> 0,
+                              () -> shotCalculator.calculateShot().targetHeading()))));
     }
     registerPathplannerCommand(
         "run flywheel kicker",
