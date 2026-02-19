5ef19b5df61514d22ddb513217975494f1c633f1
DelgLe <37987641+DelgLe@users.noreply.github.com>
2026-02-18 21:59:57 -0800

    Update simulation motor IDs and remove unused import
    Adjust several SimulationConstants motor IDs to new values and tidy up code: INTAKE_MOTOR 12->30, INDEXER_MOTOR 9->31, FLYWHEEL_MOTOR 2->32, HOOD_MOTOR 6->34. Add a FIXME note in PracticeBotConstants to indicate values need to be updated to real hardware IDs. Remove unused import of PracticeBotConstants from FlywheelKickerIOPB.

--- src/main/java/frc/robot/subsystems/FlywheelKicker/FlywheelKickerIOPB.java
+++ src/main/java/frc/robot/subsystems/FlywheelKicker/FlywheelKickerIOPB.java
@@ -13,7 +13,6 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import frc.robot.Constants;
-import frc.robot.Constants.PracticeBotConstants;
 
 public class FlywheelKickerIOPB implements FlywheelKickerIO {
   /** Creates a new FlywheelKickerIOPB. */
