c93588ed415c02b4ea5148f0ff09ef2e1487f507
xparker <29kimowen@bprep.org>
2026-02-21 12:52:19 -0800

    finished shotCalculator testing?

--- src/test/java/frc/TestShotCalculator.java
+++ src/test/java/frc/TestShotCalculator.java
@@ -14,22 +14,32 @@ import org.junit.jupiter.api.Test;
 
 public class TestShotCalculator {
   @Test
-  void test() {
-    ShootingParams expectedShootingParams = new ShootingParams(new Rotation2d(), 0.0, 0.0);
+  void shotCalculatorTest() {
     final InterpolatingDoubleTreeMap interpolatingTreeMapTestZero =
         new InterpolatingDoubleTreeMap();
-    interpolatingTreeMapTestZero.put(0.0, 0.0);
-    ShotCalculator zeroShotCalculator =
+    interpolatingTreeMapTestZero.put(1.0, 0.0);
+    ShotCalculator testShotCalculator =
         new ShotCalculator(
-            () -> new Pose2d(),
+            // public Pose2d(double x, double y, Rotation2d rotation)
+            () -> new Pose2d(1.0, 0.0, new Rotation2d()),
             () -> new Translation2d(),
             interpolatingTreeMapTestZero,
             interpolatingTreeMapTestZero,
             new Transform2d());
-    ShootingParams cachedShootingParams = zeroShotCalculator.calculateShot();
+    ShootingParams cachedShootingParams = testShotCalculator.calculateShot();
 
     assertEquals(0.0, cachedShootingParams.hoodAngle());
+
     assertEquals(0.0, cachedShootingParams.flywheelSpeed());
+
+    System.out.println(cachedShootingParams.targetHeading().getDegrees());
+    System.out.println(new Rotation2d().getDegrees());
     assertTrue(cachedShootingParams.targetHeading().equals(new Rotation2d()));
+
+    ShootingParams cachedShootingParams2 = testShotCalculator.calculateShot();
+
+    assertEquals(0.0, cachedShootingParams2.hoodAngle());
+    assertEquals(0.0, cachedShootingParams2.flywheelSpeed());
+    assertTrue(cachedShootingParams2.targetHeading().equals(new Rotation2d()));
   }
 }
