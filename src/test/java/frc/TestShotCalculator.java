1d0c6142ca2c1386a9c7c471dd1c9889e246079c
Okim29 <29kimowen@bprep.org>
2026-02-21 17:01:12 -0800

    Update TestShotCalculator.java

--- src/test/java/frc/TestShotCalculator.java
+++ src/test/java/frc/TestShotCalculator.java
@@ -1,6 +1,7 @@
 package frc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -62,17 +63,48 @@ public class TestShotCalculator {
             new Transform2d(),
             0.0,
             5.0);
-    Supplier<Pose2d> testPose = () -> new Pose2d(1.0, 0.0, new Rotation2d());
+    Pose2d testPose = new Pose2d(1.0, 0.0, new Rotation2d());
+    Supplier<Pose2d> testPoseSupplier = () -> testPose;
     ShotCalculator testShotCalculator =
-        new ShotCalculator(
-            // public Pose2d(double x, double y, Rotation2d rotation)
-            testPose, () -> new Translation2d(), RobotShootingInfo);
+        new ShotCalculator(testPoseSupplier, () -> new Translation2d(), RobotShootingInfo);
     ShootingParams cachedShootingParams = testShotCalculator.calculateShot();
 
-    testPose = () -> (new Pose2d(430987523953.984213, 9832572384.3249874, new Rotation2d()));
+    testPose.plus(new Transform2d(430987523953.984213, 9832572384.3249874, new Rotation2d()));
 
     ShootingParams cashedShootingParams2 = testShotCalculator.calculateShot();
 
     assertEquals(cachedShootingParams, cashedShootingParams2);
+  }
+
+  @Test
+  void shotCalculatorTestCacheCleared() {
+    final InterpolatingDoubleTreeMap interpolatingTreeMapTest1 = new InterpolatingDoubleTreeMap();
+    final InterpolatingDoubleTreeMap interpolatingTreeMapTest2 = new InterpolatingDoubleTreeMap();
+    final InterpolatingDoubleTreeMap interpolatingTreeMapTest3 = new InterpolatingDoubleTreeMap();
+    interpolatingTreeMapTest1.put(1.0, 0.0);
+    interpolatingTreeMapTest2.put(3.5, 2.0);
+    interpolatingTreeMapTest3.put(2.3, 4.25);
+
+    RobotShootingInfo RobotShootingInfo =
+        new RobotShootingInfo(
+            interpolatingTreeMapTest1,
+            interpolatingTreeMapTest2,
+            interpolatingTreeMapTest3,
+            new Transform2d(),
+            0.0,
+            5.0);
+    Pose2d testPose = new Pose2d(1.0, 0.0, new Rotation2d());
+    Supplier<Pose2d> testPoseSupplier = () -> testPose;
+    ShotCalculator testShotCalculator =
+        new ShotCalculator(testPoseSupplier, () -> new Translation2d(), RobotShootingInfo);
+    ShootingParams cachedShootingParams = testShotCalculator.calculateShot();
+
+    testShotCalculator.clearShootingParams();
+    testPose.plus(new Transform2d(430987523953.984213, 9832572384.3249874, new Rotation2d()));
+
+    ShootingParams cachedShootingParams2 = testShotCalculator.calculateShot();
+    System.out.println(cachedShootingParams2);
+
+    assertNotEquals(cachedShootingParams, cachedShootingParams2);
   }
 }
