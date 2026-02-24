4e41fcc77ad1fcfcbe473fa83df03c9bf860033e
27MahoneyKeirnan <ilikebuffalowingsalot@gmail.com>
2026-02-23 18:37:10 -0800

    Added ClimberIONoop.java

---
+++ src/main/java/frc/robot/subsystems/Climber/ClimberIONoop.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Climber;
+
+public class ClimberIONoop implements ClimberIO {
+  public void setZero() {}
+
+  public void setPosition(double position) {}
+
+  public void setDutyCycle(double value) {}
+
+  public void enableBrakeMode() {}
+
+  public void disableBrakeMode() {}
+
+  public void setLeftDutyCycle(double dutyCycle) {}
+
+  public void setLeftPosition(double position) {}
+
+  public void setRightDutyCycle(double dutyCycle) {}
+
+  public void setRightPosition(double position) {}
+
+  public boolean leftAboveMinHeight() {
+      return false;}
+
+  public boolean rightAboveMinHeight() {
+      return false;}
+  
+}
