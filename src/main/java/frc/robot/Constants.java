706e5d6403e0ba5d8e41e8fc809743ca5b970c9b
xparker <ilikebuffalowingsalot@gmail.com>
2026-03-03 00:34:35 -0800

    getRobotType to main (#425)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,6 +43,27 @@ public final class Constants {
     REPLAY
   }
 
+  static RobotType robotType;
+
+  public static RobotType getRobotType() {
+    return robotType;
+  }
+
+  static RobotType initRobotType() {
+    String serialAddress = HALUtil.getSerialNumber();
+
+    if (serialAddress.equals(SerialAddressConstants.WOOD_SERIAL_ADDRESS)) {
+      robotType = Constants.RobotType.WOODBOT;
+    } else if (serialAddress.equals(SerialAddressConstants.PRACTICE_SERIAL_ADDRESS)) {
+      robotType = Constants.RobotType.PRACTICEBOT;
+    } else if (!Robot.isReal()) { // KEEP AT BOTTOM
+      robotType = Constants.RobotType.SIM;
+    } else {
+      robotType = Constants.RobotType.PRACTICEBOT;
+    }
+    return robotType;
+  }
+
   public static final class IOConstants {
     // === USB PATHS ===
     public static final String USB_ROOT_DIRECTORY = "/U";
@@ -329,17 +350,4 @@ public final class Constants {
   }
 
   public static double loopPeriodSecs; // add value
-
-  public static RobotType getRobotType() {
-    String serialAddress = HALUtil.getSerialNumber();
-
-    if (serialAddress.equals(SerialAddressConstants.WOOD_SERIAL_ADDRESS)) {
-      return Constants.RobotType.WOODBOT;
-    } else if (serialAddress.equals(SerialAddressConstants.PRACTICE_SERIAL_ADDRESS)) {
-      return Constants.RobotType.PRACTICEBOT;
-    } else if (!Robot.isReal()) { // KEEP AT BOTTOM
-      return Constants.RobotType.SIM;
-    }
-    return Constants.RobotType.PRACTICEBOT;
-  }
 }
