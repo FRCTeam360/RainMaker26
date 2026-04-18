cbea653bb44fd3bbe87739d2476637a407fc8243
Ashutosh Tiwari <ashutoshtiwari200128@gmail.com>
2026-04-18 23:28:05 +0530

    Make ShooterConstants fields final

--- src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -14,12 +14,12 @@ public class ShooterConstants {
   public static final double ALLIANCE_PHASE_DURATION_SECONDS = 25.0;
   public static final double ALLIANCE_PHASE_START_MODULO = 5.0;
 
-  public static Transform2d PRACTICEBOT_TO_SHOOTER =
+  public static final Transform2d PRACTICEBOT_TO_SHOOTER =
       new Transform2d(Units.inchesToMeters(-7.437), 0.0, Rotation2d.k180deg);
 
-  public static Transform2d COMPBOT_TO_SHOOTER =
+  public static final Transform2d COMPBOT_TO_SHOOTER =
       new Transform2d(Units.inchesToMeters(-7.437), 0.0, Rotation2d.k180deg);
 
-  public static Transform2d SIM_TO_SHOOTER =
+  public static final Transform2d SIM_TO_SHOOTER =
       new Transform2d(Units.inchesToMeters(-7.437), 0.0, Rotation2d.k180deg);
 }
