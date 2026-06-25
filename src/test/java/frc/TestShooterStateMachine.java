646b5d7cbd19e67ff2fbd2c9b2fcb7570ad476bb
Okim29 <29kimowen@bprep.org>
2026-06-24 19:54:11 -0700

    mitosis

--- src/test/java/frc/TestShooterStateMachine.java
+++ src/test/java/frc/TestShooterStateMachine.java
@@ -9,16 +9,22 @@ import org.junit.jupiter.api.Test;
 
 public class TestShooterStateMachine {
   @Test
-  void TestSetWantedStateAndStandbyVersusWaiting() {
+  void TestSetWantedStateStandby() {
     ShooterStateMachine testShooterStateMachine =
         new ShooterStateMachine(null, null, null, null, null);
     testShooterStateMachine.setIsInAllianceZoneSupplier(() -> true);
     testShooterStateMachine.setWantedState(ShooterWantedStates.PASSIVE_SHOOTER);
     testShooterStateMachine.update();
     assertEquals(ShooterStates.STANDBY, testShooterStateMachine.getState());
+  }
+  @Test
+  void TestSetWantedStateWaiting() {
+    ShooterStateMachine testShooterStateMachine =
+        new ShooterStateMachine(null, null, null, null, null);
     testShooterStateMachine.setIsInAllianceZoneSupplier(() -> false);
     testShooterStateMachine.setWantedState(ShooterWantedStates.PASSIVE_SHOOTER);
     testShooterStateMachine.update();
     assertEquals(ShooterStates.WAITING, testShooterStateMachine.getState());
   }
+  //put stuff for more states ig
 }
