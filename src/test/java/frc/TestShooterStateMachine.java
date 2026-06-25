fc4a7abfc14324b7c6687e4fc190de913434be0f
Okim29 <29kimowen@bprep.org>
2026-06-24 19:14:31 -0700

    first shooter state machine test

---
+++ src/test/java/frc/TestShooterStateMachine.java
@@ -0,0 +1,24 @@
+package frc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import frc.robot.subsystems.Shooter.ShooterStateMachine;
+import frc.robot.subsystems.Shooter.ShooterStateMachine.ShooterStates;
+import frc.robot.subsystems.Shooter.ShooterStateMachine.ShooterWantedStates;
+import org.junit.jupiter.api.Test;
+
+public class TestShooterStateMachine {
+  @Test
+  void TestSetWantedStateAndUpdate() {
+    ShooterStateMachine testShooterStateMachine =
+        new ShooterStateMachine(null, null, null, null, null);
+    testShooterStateMachine.setIsInAllianceZoneSupplier(() -> true);
+    testShooterStateMachine.setWantedState(ShooterWantedStates.PASSIVE_SHOOTER);
+    testShooterStateMachine.update();
+    assertEquals(ShooterStates.STANDBY, testShooterStateMachine.getState());
+    testShooterStateMachine.setIsInAllianceZoneSupplier(() -> false);
+    testShooterStateMachine.setWantedState(ShooterWantedStates.PASSIVE_SHOOTER);
+    testShooterStateMachine.update();
+    assertEquals(ShooterStates.WAITING, testShooterStateMachine.getState());
+  }
+}
